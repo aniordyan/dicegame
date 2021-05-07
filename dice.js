0c27710c6211a3fe0bd124d08270bbed266d86e8
aniordyan <77895035+aniordyan@users.noreply.github.com>
2021-05-07 22:37:36 +0400

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,27 @@
+
+function rollTheDice() {
+    setTimeout(function () {
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+        document.querySelector(".img1").setAttribute("src",
+            "dice" + randomNumber1 + ".png");
+
+        document.querySelector(".img2").setAttribute("src",
+            "dice" + randomNumber2 + ".png");
+
+        if (randomNumber1 === randomNumber2) {
+            document.querySelector("h1").innerHTML = "Հավասար է ";
+        }
+
+        else if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML
+                            = ("Երկրորդը հաղթեց");
+        }
+
+        else {
+          document.querySelector("h1").innerHTML
+                            = ("Առաջինը հաղթեց");
+        }
+    });
+}
